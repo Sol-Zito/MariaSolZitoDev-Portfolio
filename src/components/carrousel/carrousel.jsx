c3d4c5818099b660699dc84a12d72832418f1eda
Sol <solzito.1889@gmail.com>
2025-05-21 12:28:33 -0300

    se crea componente de carousel

---
+++ src/components/carrousel/carrousel.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const Carousel = (items) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + items.length) % items.length
+    );
+  };
+
+  return (
+    <div className="relative w-full max-w-3xl mx-auto">
+      <div className="overflow-hidden relative h-64">
+        {items.map((image, index) => (
+          <div
+            key={index}
+            className={`absolute inset-0 transition-transform transform ${
+              index === currentIndex ? "translate-x-0" : "translate-x-full"
+            }`}
+          >
+            <img
+              src={image}
+              alt={`Slide ${index}`}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+      <button
+        className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-800 text-white p-2"
+        onClick={prevSlide}
+      >
+        Prev
+      </button>
+      <button
+        className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-800 text-white p-2"
+        onClick={nextSlide}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
