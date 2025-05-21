1d6c0e1c421dffeb2b24d8a630139377ad40a25f
Sol <solzito.1889@gmail.com>
2025-05-21 12:26:27 -0300

    se refactoriza componente contact

--- src/contacto/Contact.jsx
+++ src/contacto/Contact.jsx
@@ -6,56 +6,17 @@ import { Wpp } from "../utils/IconWpp";
 
 const Contact = () => {
   return (
-    <div className="bg-cardB w-5/6 m-auto p-4">
+    <div id="contact" className="border bg-cardB m-auto p-2">
       <Title name="Contact" />
-      <div className="bg-background text-center items-center m-auto p-2">
-        <p>
-          <strong>Zito Maria Sol</strong>
-        </p>
-        <div className="grid grid-cols-2 gap-2 pt-4 w-4/5 m-auto">
-          <p className="flex">
-            {Wpp("black")}
-            <a
-              href="tel:+5493415623126"
-              target="_blank"
-              className="hidden md:flex"
-            >
-              : +5493415623126
-            </a>
-          </p>
-          <p className="flex">
-            {Linkedin("black")}
-            <a
-              href="http://www.linkedin.com/in/solzito1998"
-              target="_blank"
-              className="hidden md:flex"
-            >
-              : www.linkedin.com/in/solzito1998
-            </a>
-          </p>
-          <p className="flex">
-            {Github("black")}
-            <a
-              href="https://github.com/Sol-Zito"
-              target="_blank"
-              className="hidden md:flex"
-            >
-              : github.com/Sol-Zito
-            </a>{" "}
-          </p>
-          <p className="flex">
-            {email}
-            <a
-              href="mailto:solzito.1889@gmail.com?subject=Asunto%20del%20correo&body=Hola,%20esta%20es%20una%20prueba."
-              className="hidden md:flex"
-            >
-              : solzito.1889@gmail.com
-            </a>
-          </p>
+      <div className="bg-background border text-center items-center m-auto py-4">
+        <div className="flex flex-cols gap-4 justify-center m-auto pb-2">
+          <p>{Wpp("black")}</p>
+          <p>{Linkedin("black")}</p>
+          <p>{Github("black")}</p>
+          <p>{email}</p>
         </div>
-      </div>
-      <div>
-        <p className="text-background py-2">
+        <hr />
+        <p className="w-4/5 m-auto p-2">
           Thank you for taking the time to view my portfolio. Any advice is more
           than welcome. Above are all my contact information, should you have
           any questions. Best regards!
